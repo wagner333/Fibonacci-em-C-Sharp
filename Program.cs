5918963e202db1cf9d14153ad6c847803c3b8b79
Wagner <wwfome6@gmail.com>
2025-06-05 22:55:17 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace program
+{
+    public class Program
+    {
+        private int quantidade;
+        private int fib1;
+        private int fib2;
+
+        public Program()
+        {
+            quantidade = 10;
+            fib1 = 0;
+            fib2 = 1;
+        }
+
+        public void ExecutarFibonacci()
+        {
+            for (int i = 1; i <= quantidade; i++)
+            {
+                int fib3 = fib1 + fib2;
+                Console.WriteLine(fib3);
+                fib1 = fib2;
+                fib2 = fib3;
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.ExecutarFibonacci(); // Chama o método que executa a lógica
+        }
+    }
+}
